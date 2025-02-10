94114cbdbcf215cbf9168350203d52a10ac56221
dot-ruth <ruthgetaneh5@gmail.com>
2025-02-10 14:46:03 +0300

    FEAT|#3| Implemented bottom navigation

--- expense_tracker/lib/screens/home/views/home_screen.dart
+++ expense_tracker/lib/screens/home/views/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +8,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 3,
+        items:const [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label:'Home'),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.graph_square_fill),label:'Stats'),
+      ])),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.tertiary,
+            ],
+            transform:const GradientRotation(pi / 4),
+            )
+        ) ,
+        child: const Icon(CupertinoIcons.add),
+        )),
+    );
   }
 }
